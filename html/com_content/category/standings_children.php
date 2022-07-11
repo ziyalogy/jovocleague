0e5e0d7f9d5eb0f1c1775529c6accbcaf88962cb
ziyalogy <ziyalogy@gmail.com>
2022-07-11 21:12:14 +0300

    League table table

--- html/com_content/category/standings_children.php
+++ html/com_content/category/standings_children.php
@@ -3,83 +3,80 @@
  * @package     Joomla.Site
  * @subpackage  com_content
  *
- * @copyright   Copyright (C) 2005 - 2017 Open Source Matters, Inc. All rights reserved.
+ * @copyright   (C) 2009 Open Source Matters, Inc. <https://www.joomla.org>
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 defined('_JEXEC') or die;
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
+use Joomla\CMS\Router\Route;
+use Joomla\Component\Content\Site\Helper\RouteHelper;
 
-JHtml::_('bootstrap.tooltip');
+$lang   = Factory::getLanguage();
+$user   = Factory::getUser();
+$groups = $user->getAuthorisedViewLevels();
+?>
 
-$class = ' class="first"';
-$lang  = JFactory::getLanguage();
-
-if ($this->maxLevel != 0 && count($this->children[$this->category->id]) > 0) : ?>
-
+<?php if (count($this->children[$this->category->id]) > 0) : ?>
 	<?php foreach ($this->children[$this->category->id] as $id => $child) : ?>
-		<?php
-		if ($this->params->get('show_empty_categories') || $child->numitems || count($child->getChildren())) :
-			if (!isset($this->children[$this->category->id][$id + 1])) :
-				$class = ' class="last"';
-			endif;
-		?>
-		<div<?php echo $class; ?>>
-			<?php $class = ''; ?>
-			<?php if ($lang->isRtl()) : ?>
-			<h3 class="page-header item-title">
-				<?php if ( $this->params->get('show_cat_num_articles', 1)) : ?>
-					<span class="badge badge-info tip hasTooltip" rel="tooltip" title="<?php echo JHtml::_('tooltipText', 'COM_CONTENT_NUM_ITEMS_TIP'); ?>">
-						<?php echo $child->getNumItems(true); ?>
-					</span>
+		<?php // Check whether category access level allows access to subcategories. ?>
+		<?php if (in_array($child->access, $groups)) : ?>
+			<?php if ($this->params->get('show_empty_categories') || $child->getNumItems(true) || count($child->getChildren())) : ?>
+			<div class="com-content-category__children">
+				<?php if ($lang->isRtl()) : ?>
+				<h3 class="page-header item-title">
+					<?php if ( $this->params->get('show_cat_num_articles', 1)) : ?>
+						<span class="badge bg-info tip hasTooltip" title="<?php echo HTMLHelper::_('tooltipText', 'COM_CONTENT_NUM_ITEMS'); ?>">
+							<?php echo $child->getNumItems(true); ?>
+						</span>
+					<?php endif; ?>
+					<a href="<?php echo Route::_(RouteHelper::getCategoryRoute($child->id, $child->language)); ?>">
+					<?php echo $this->escape($child->title); ?></a>
+
+					<?php if (count($child->getChildren()) > 0 && $this->maxLevel > 1) : ?>
+						<a href="#category-<?php echo $child->id; ?>" data-bs-toggle="collapse" data-toggle="button" class="btn btn-sm float-end" aria-label="<?php echo Text::_('JGLOBAL_EXPAND_CATEGORIES'); ?>"><span class="icon-plus" aria-hidden="true"></span></a>
+					<?php endif; ?>
+				</h3>
+				<?php else : ?>
+				<h3 class="page-header item-title"><a href="<?php echo Route::_(RouteHelper::getCategoryRoute($child->id, $child->language)); ?>">
+					<?php echo $this->escape($child->title); ?></a>
+					<?php if ( $this->params->get('show_cat_num_articles', 1)) : ?>
+						<span class="badge bg-info tip hasTooltip" title="<?php echo HTMLHelper::_('tooltipText', 'COM_CONTENT_NUM_ITEMS'); ?>">
+							<?php echo $child->getNumItems(true); ?>
+						</span>
+					<?php endif; ?>
+
+					<?php if (count($child->getChildren()) > 0 && $this->maxLevel > 1) : ?>
+						<a href="#category-<?php echo $child->id; ?>" data-bs-toggle="collapse" data-toggle="button" class="btn btn-sm float-end" aria-label="<?php echo Text::_('JGLOBAL_EXPAND_CATEGORIES'); ?>"><span class="icon-plus" aria-hidden="true"></span></a>
+					<?php endif; ?>
+				</h3>
 				<?php endif; ?>
-				<a href="<?php echo JRoute::_(ContentHelperRoute::getCategoryRoute($child->id)); ?>">
-				<?php echo $this->escape($child->title); ?></a>
-
-				<?php if ($this->maxLevel > 1 && count($child->getChildren()) > 0) : ?>
-					<a href="#category-<?php echo $child->id;?>" data-toggle="collapse" data-toggle="button" class="btn btn-default btn-xs pull-right"><span class="fa fa-plus"></span></a>
-				<?php endif;?>
-			</h3>
-			<?php else : ?>
-			<h3 class="page-header item-title"><a href="<?php echo JRoute::_(ContentHelperRoute::getCategoryRoute($child->id));?>">
-				<?php echo $this->escape($child->title); ?></a>
-				<?php if ( $this->params->get('show_cat_num_articles', 1)) : ?>
-				<span class="badge badge-info tip hasTooltip" title="<?php echo JHtml::_('tooltipText', 'COM_CONTENT_NUM_ITEMS_TIP'); ?>">
-					<?php echo Text::_('COM_CONTENT_NUM_ITEMS'); ?>
-					<?php echo $child->getNumItems(true); ?>
-				</span>
-				<?php endif ; ?>
-				
-				<?php if ($this->maxLevel > 1 && count($child->getChildren()) > 0) : ?>
-					<a href="#category-<?php echo $child->id;?>" data-toggle="collapse" data-toggle="button" class="btn btn-default btn-xs pull-right"><span class="fa fa-plus"></span></a>
-				<?php endif;?>
-			<?php endif;?>
-			</h3>
-
-			<?php if ($this->maxLevel > 1 && count($child->getChildren()) > 0) : ?>
-				<?php if ($child->description) : ?>
-					<div class="category-desc">
-						<?php echo JHtml::_('content.prepare', $child->description, '', 'com_content.category'); ?>
-					</div>
+				<?php if ($this->params->get('show_subcat_desc') == 1) : ?>
+					<?php if ($child->description) : ?>
+						<div class="category-desc">
+							<?php echo HTMLHelper::_('content.prepare', $child->description, '', 'com_content.category'); ?>
+						</div>
+					<?php endif; ?>
 				<?php endif; ?>
-			<?php endif; ?>
 
-			<?php if (count($child->getChildren()) > 0) : ?>
-			<div class="collapse fade" id="category-<?php echo $child->id; ?>">
-				<?php
-				$this->children[$child->id] = $child->getChildren();
-				$this->category = $child;
-				$this->maxLevel--;
-				if ($this->maxLevel != 0) :
+				<?php if (count($child->getChildren()) > 0 && $this->maxLevel > 1) : ?>
+				<div class="collapse fade" id="category-<?php echo $child->id; ?>">
+					<?php
+					$this->children[$child->id] = $child->getChildren();
+					$this->category = $child;
+					$this->maxLevel--;
 					echo $this->loadTemplate('children');
-				endif;
-				$this->category = $child->getParent();
-				$this->maxLevel++;
-				?>
+					$this->category = $child->getParent();
+					$this->maxLevel++;
+					?>
+				</div>
+				<?php endif; ?>
+
 			</div>
 			<?php endif; ?>
-		</div>
 		<?php endif; ?>
 	<?php endforeach; ?>
-
-<?php endif;
+<?php endif; ?>
